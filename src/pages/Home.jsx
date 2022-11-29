d7d03eee6462575ae43bd06bbf26d5e2daa2b66d
vjvl95 <vjvl95@naver.com>
2022-11-30 02:57:40 +0900

    refactor: 공통으로 쓰이는 컴포넌트 분리

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,25 @@
+import BlackBox from 'component/common/BlackBox';
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 export default function Home() {
   return (
     <>
       <Background>
-        <Title>
+        <BlackBox>
           2023년
           <br />
           ♪새해 첫 곡 럭키 슬롯♪
-        </Title>
+        </BlackBox>
         <SubTitle>
           당신에게 행운을 가져다 줄 새해 첫 곡,
           <br />
           럭키 슬롯에서 뽑아보세요!
         </SubTitle>
+        <StartContaier>
+          <NavLink to='test'>
+            <StartButton>시작하기</StartButton>
+          </NavLink>
+        </StartContaier>
       </Background>
     </>
   );
@@ -21,26 +28,35 @@ export default function Home() {
 const Background = styled.div`
   width: calc(100%-40px);
   height: 200px;
-  margin: 20px;
-  padding-top: 10px;
+  margin: 25px;
+  padding-top: 15px;
   box-shadow: 0px 4px 15px rgba(0, 165, 85, 0.15);
   border-radius: 15px;
 `;
-const Title = styled.div`
-  width: 80%;
-  margin: 0px auto;
-  height: 80px;
-  background-color: black;
-  border-radius: 15px;
-  color: white;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-`;
+
 const SubTitle = styled.div`
   margin-top: 40px;
   display: flex;
   justify-content: center;
+  text-align: center;
+  line-height: 23px;
+`;
+
+const StartButton = styled.div`
+  width: 190px;
+  height: 64px;
+  display: inline-block;
+  background: #00c981;
+  backdrop-filter: blur(2.5px);
+  border-radius: 100px;
+  margin-top: 60px;
+  text-align: center;
+  line-height: 55px;
+  color: white;
+  font-weight: 800;
+`;
+const StartContaier = styled.div`
+  width: 100%;
+
   text-align: center;
 `;
