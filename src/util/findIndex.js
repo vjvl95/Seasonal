b63b10c53faf1dd65c532f9fa8448c210917bfe8
vjvl95 <vjvl95@naver.com>
2022-12-02 01:38:12 +0900

    fact: 랜덤으로 숫자를 뽑아서 index를 돌려주는 함수

---
+++ src/util/findIndex.js
@@ -0,0 +1,16 @@
+import { imgFileLeft, imgFileRight, slot } from 'component/common/const';
+
+export default function findIndex() {
+  const randomNumber = Math.trunc(Math.random() * 50) + 1;
+  console.log(randomNumber);
+  const index = slot[randomNumber].map((v, i) => {
+    if (i === 0) {
+      return -imgFileLeft.indexOf(v);
+    }
+    if (i === 1) {
+      return -imgFileRight.indexOf(v);
+    }
+    return v;
+  });
+  return index;
+}
