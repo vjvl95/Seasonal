37f8d55e99c89794d565beff47cb048bbb09ba6b
vjvl95 <vjvl95@naver.com>
2022-12-01 18:26:52 +0900

    feat: 이미지 추가

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,16 @@
 import { Navigate, Route, Routes, Outlet } from 'react-router-dom';
 import Layout from '../layouts/Layout';
-import { Home, Name, Category } from '../pages';
+import { Home, Name, Category, SlotMachine, Test } from '../pages';
 export default function index() {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path='/test' element={<Name />} />
+        <Route path='/name' element={<Name />} />
         <Route path='/selectCategory' element={<Category />} />
+        <Route path='/slotMachine' element={<SlotMachine />} />
+        <Route path='/test' element={<Test />} />
+
         {/* <Route path='/test' element={<Test />} />
         <Route path='/test/slice' element={<SliceTest />} />
         <Route path='/write' element={<Editor />} /> */}
