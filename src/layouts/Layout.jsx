0075e090bbe6a875746299eb521abafc5838e80c
vjvl95 <vjvl95@naver.com>
2022-12-08 17:45:29 +0900

    refactor: Layout 반응형에 맞게끔 변경

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -13,34 +13,40 @@ export default function Layout() {
   );
 }
 
-const Root = styled.div`
-  height: 100vh;
-`;
+const Root = styled.div``;
 const Container = styled.div`
   margin: 0 auto;
   width: 50%;
-  height: 100%;
+  height: 100vh;
   @media screen and (max-width: 601px) {
     width: 100%;
   }
 `;
+
 const Top = styled.div`
   width: 100%;
   background: linear-gradient(#cef1c4, white);
   margin-bottom: 60px;
   height: 15%;
+
   @media screen and (max-width: 601px) {
     margin-bottom: 30px;
   }
 `;
 
 const Bottom = styled.div`
-  width: 100%;
+  position: absolute;
+  bottom: 0px;
+
+  width: 50%;
   height: 15%;
   background: linear-gradient(white, #cef1c4);
   @media screen and (max-width: 601px) {
     position: fixed;
     bottom: 0px;
     width: 100%;
+  }
+  @media screen and (max-height: 800px) {
+    display: none;
   }
 `;
