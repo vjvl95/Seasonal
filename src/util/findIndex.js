b0f30cf2a750ed2f01e4fde73b5cd3ddcf045400
vjvl95 <vjvl95@naver.com>
2022-12-08 17:42:34 +0900

    feat: 랜덤으로 슬롯 뽑기 함수 완성

--- src/util/findIndex.js
+++ src/util/findIndex.js
@@ -1,16 +1,39 @@
 import { imgFileLeft, imgFileRight, category, slot } from 'component/common/const';
 
-export default function findIndex() {
-  const randomNumber = Math.trunc(Math.random() * 50) + 1;
-  console.log(randomNumber);
-  const index = slot[randomNumber].map((v, i) => {
-    if (i === 0) {
-      return -imgFileLeft.indexOf(v);
-    }
-    if (i === 1) {
-      return -imgFileRight.indexOf(v);
-    }
-    return -category.indexOf(v);
+export default function findIndex(selectCategory) {
+  const select = slot.filter((v) => {
+    if (v[3] === selectCategory) return v;
   });
+  console.log(select);
+  const randomNumber = Math.trunc(Math.random() * select.length);
+  console.log(randomNumber);
+  const slotIndex = slot
+    .map((v, i) => {
+      if (
+        select[randomNumber][0] === v[0] &&
+        select[randomNumber][1] === v[1] &&
+        select[randomNumber][2] === v[2]
+      ) {
+        return i;
+      }
+    })
+    .filter((v) => v !== undefined);
+
+  console.log(slotIndex[0]);
+  console.log(slot[slotIndex[0]]);
+  const index = slot[slotIndex[0]]
+    .map((v, i) => {
+      if (i === 0) {
+        return -imgFileLeft.indexOf(v);
+      }
+      if (i === 1) {
+        return -imgFileRight.indexOf(v);
+      }
+      if (i === 2) {
+        return -category.indexOf(v);
+      }
+    })
+    .filter((v) => v !== undefined);
+  console.log(index);
   return index;
 }
