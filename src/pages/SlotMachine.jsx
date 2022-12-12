6edddcf97efc85b2007569ff53bfcce1a1d752d2
vjvl95 <vjvl95@naver.com>
2022-12-12 12:39:40 +0900

    refactor: 애니메이션 끝나면 결과창으로 이동가능하게 변경

--- src/pages/SlotMachine.jsx
+++ src/pages/SlotMachine.jsx
@@ -1,5 +1,5 @@
 import BlackBox from 'component/common/BlackBox';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import lever from '../img/lever.png';
 import resultPrint from '../img/resultPrint.png';
 import SlotMachineBox from '../component/common/SlotComponent';
@@ -7,21 +7,42 @@ import styled, { keyframes, css } from 'styled-components';
 import { NavLink, useLocation } from 'react-router-dom';
 import findIndex from 'util/findIndex';
 import { slot } from 'component/common/const';
+import { buttonEmoticon } from 'img';
+import { useEffect } from 'react';
+
 export default function SlotMachine() {
   const [isClick, setIsClick] = useState(false);
+  const [endAnimation, setEndAnimation] = useState(false);
   const location = useLocation();
   const { state } = location;
-  console.log(state);
-  const slotIndex = isClick ? findIndex(state.category) : '';
+  const slotIndex = useMemo(() => findIndex(state.category), [state]);
+
+  useEffect(() => {
+    if (isClick) {
+      setTimeout(() => {
+        setEndAnimation(true);
+      }, 7000);
+    }
+  }, [isClick]);
 
   return (
     <>
       <Background>
+        <BlackBox fontSize={'15px'} paddingTop={'15px'} isFlex={false}>
+          {isClick ? (
+            <>
+              <span style={{ fontWeight: '700' }}>{state.name}</span>님에게 <br />
+              2023년 <span style={{ fontWeight: '700' }}>{state.category}</span>을 가져다 줄
+              노래는..?!
+            </>
+          ) : (
+            <>
+              <span style={{ fontWeight: '700' }}>{state.name}</span> 님의 <br />
+              새해 첫 곡을 찾아줄 럭키 슬롯!
+            </>
+          )}
+        </BlackBox>
         <SlotBox>
-          <BlackBox>
-            가나다라마바사아자차카다 님의 <br />
-            새해 첫 곡을 찾아줄 럭키 슬롯
-          </BlackBox>
           <SlotMachineBox
             isClick={isClick}
             setIsClick={setIsClick}
@@ -30,16 +51,38 @@ export default function SlotMachine() {
           />
         </SlotBox>
         <ButtonBox>
+          {!isClick && <LeverText>손잡이를 내려 럭키슬롯을 돌려주세요</LeverText>}
           <Lever isClicked={isClick} onClick={() => setIsClick(!isClick)} src={lever}></Lever>
           {isClick && (
             <>
-              <img src={resultPrint} width={'250px'} alt='' />
-              <NavLink
-                to={`/result/${slot[slotIndex[3]][4]}`}
-                state={{ slot: slot[slotIndex[3]], name: state.name }}
-              >
-                <StartButton>확인해보기!</StartButton>
-              </NavLink>
+              <img
+                src={resultPrint}
+                style={{ marginBottom: '15px', marginTop: '15px' }}
+                width={'250px'}
+                alt=''
+              />
+              <AfterButton>
+                <img src={buttonEmoticon} width={'95px'} height={'108px'} alt=''></img>
+                <NavLink
+                  to={`/result/${slot[slotIndex[3]][4]}`}
+                  state={{ slot: slot[slotIndex[3]], name: state.name }}
+                >
+                  {endAnimation ? (
+                    <StartButton>확인해보기!</StartButton>
+                  ) : (
+                    <StartButton disabled endAnimation={endAnimation}>
+                      결과를 기다려주세요
+                    </StartButton>
+                  )}
+                </NavLink>
+                <img
+                  style={{ transform: 'matrix(-1, 0, 0, 1, 0, 0)' }}
+                  src={buttonEmoticon}
+                  alt=''
+                  width={'95px'}
+                  height={'108px'}
+                ></img>
+              </AfterButton>
             </>
           )}
         </ButtonBox>
@@ -47,57 +90,53 @@ export default function SlotMachine() {
     </>
   );
 }
-
+const AfterButton = styled.div`
+  display: flex;
+`;
 const scroll2 = keyframes`
- from{
-  transform: translateZ(0);
- }
 
 	to {
 		opacity: 0;
     visibility: hidden;
     height:0px;
-    transform: translate3d(-500%, 0, 0);
-	}
-`;
-const scroll = keyframes`
- from {
-		opacity: 0;
-	}
-	to {
-		opacity: 1;
 	}
 `;
 
+const LeverText = styled.div`
+  font-weight: 400;
+  font-size: 10px;
+  line-height: 20px;
+  color: #00c981;
+  margin-top: 10px;
+  margin-bottom: 15px;
+`;
+
 const Background = styled.div`
-  padding-top: 10%;
-  width: 100%;
-  box-shadow: 0px -4px 10px rgba(0, 165, 85, 0.1);
-  border-radius: 125px 125px 0px 0px;
-  @media screen and (max-width: 601px) {
-    padding-top: 15%;
+  min-height: 810px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 39.4%;
+
+  @media screen and (min-width: 801px) {
+    padding-top: 34.1%;
   }
 `;
 const StartButton = styled.button`
-  width: 190px;
+  width: 184px;
   height: 64px;
-  font-size: 20px;
-  display: inline-block;
+  font-size: 14px;
   background: #00c981;
   border-radius: 100px;
   text-align: center;
-  line-height: 55px;
   color: white;
-  font-weight: 800;
+  font-weight: 700;
   border: none;
-  @media screen and (min-width: 601px) {
-    margin-top: 15px;
+  line-height: 17.5px;
+  &:disabled {
+    background: gray;
+    border: 3px solid;
   }
-  ${(props) =>
-    props.isClicked &&
-    css`
-      animation: ${scroll} 5s;
-    `};
 `;
 const SlotBox = styled.div`
   align-items: center;
@@ -105,22 +144,17 @@ const SlotBox = styled.div`
   flex-direction: column;
 `;
 const ButtonBox = styled.div`
-  margin-top: 30px;
-  background: linear-gradient(#ecfcec, white);
   text-align: center;
-  height: 30%;
   display: flex;
   align-items: center;
   flex-direction: column;
-  min-height: 300px;
 `;
 const Lever = styled.img`
   width: 70px;
   height: 150px;
-  margin-top: 20px;
   ${(props) =>
     props.isClicked &&
     css`
-      animation: ${scroll2} 5s forwards;
+      animation: ${scroll2} 0s forwards;
     `};
 `;
