713d8e51f3f751894016d70dacf236f319574306
vjvl95 <vjvl95@naver.com>
2022-12-12 14:04:02 +0900

    feat: 카카오 공유하기 컨텐츠 수정

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -28,8 +28,34 @@ export default function Result() {
   }, [status]);
 
   const handleKakaoButton = () => {
-    window.Kakao.Link.sendScrap({
-      requestUrl: currentUrl,
+    window.Kakao.Link.sendDefault({
+      objectType: 'feed',
+      content: {
+        title: '제목을 여기에 씁니다.',
+        description: '내용을 여기에 씁니다',
+        imageUrl:
+          'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FeB1Yj7%2Fbtrn8HKdp01%2FlZMtAuvo986os4dCkVoAOk%2Fimg.png',
+        imageWidth: 1200,
+        imageHeight: 630,
+        link: {
+          mobileWebUrl: 'https://developers.kakao.com',
+          androidExecutionParams: 'test',
+        },
+      },
+      itemContent: {
+        profileText: '송송',
+        profileImageUrl:
+          'https://tistory1.daumcdn.net/tistory/373748/attach/af0ef0205e234b4f9f09d7bce27dd237',
+      },
+      buttons: [
+        {
+          title: '블로그 둘러보기',
+          link: {
+            mobileWebUrl: 'https://happynewyear20232.netlify.app',
+            webUrl: 'https://happynewyear20232.netlify.app',
+          },
+        },
+      ],
     });
   };
 
