215c66bdb274e9cc951962a7a501377b78ab429c
vjvl95 <vjvl95@naver.com>
2022-12-15 09:57:50 +0900

    feat: 구글 애널지스틱 코드 반영

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import { BrowserRouter } from 'react-router-dom';
 import Router from './routes';
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
+const { REACT_APP_GOOGLE_KEY } = process.env;
+
+const gaTrackingId = REACT_APP_GOOGLE_KEY; // 환경 변수에 저장된 추적ID 가져오기
+ReactGA.initialize(gaTrackingId); // react-ga 초기화 및 debug 사용
+
+const history = createBrowserHistory();
+history.listen((response) => {
+  console.log(response.location.pathname);
+  ReactGA.set({ page: response.location.pathname });
+  ReactGA.pageview(response.location.pathname);
+});
 function App() {
   return (
     <BrowserRouter>
